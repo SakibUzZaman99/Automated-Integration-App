eb147ab8565df804ed050c1bfc9eb147651db7cb
Meherin Chowdhury <ritychowdhury30@gmail.com>
2025-06-24 22:35:41 +0600

    Add files via upload

---
+++ MainAppView.swift
@@ -0,0 +1,18 @@
+//
+//  MainAppView.swift
+//  MehuAPP
+//
+//  Created by MacBook Air on 22/6/25.
+//
+
+import SwiftUI
+
+struct MainAppView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("✅ You are logged in!")
+                .font(.title)
+                .padding()
+        }
+    }
+}
